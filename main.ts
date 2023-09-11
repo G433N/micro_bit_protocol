a7202733e9ee13d8c54650bec8816f8d23a50fbc
G433N <44841986+G433N@users.noreply.github.com>
2023-09-11 11:01:27 +0200

    Paring should work, testing is next step

--- main.ts
+++ main.ts
@@ -2,32 +2,54 @@ input.onButtonPressed(Button.A, function () {
     if (STATE == "INIT") {
         music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         Endpoint = true
+        basic.showString("ENDPOINT")
     }
 })
-function Init_Radio (_receivedString: string, _serialNumber: number) {
-    if (_receivedString == "PAIR") {
-        basic.showString("" + (_serialNumber))
+function Init_Radio (_name: string, _value: number, _serialnumber: number) {
+    if (_name == "PAIR") {
+        radio.sendValue("HS", _serialnumber)
+        id = _value + 1
+        basic.showString("" + (id))
+        if (Endpoint) {
+            Switch_State("ENDPOINT")
+        } else {
+            Switch_State("PAIR")
+        }
     }
 }
 function Pair () {
-    radio.sendString("PAIR")
+    basic.pause(2000)
+    radio.sendValue("PAIR", id)
 }
 function Switch_State (_state: string) {
     STATE = _state
     basic.showString(_state)
 }
-radio.onReceivedString(function (receivedString) {
-    if (STATE == "INIT") {
-        Init_Radio(receivedString, radio.receivedPacket(RadioPacketProperty.SerialNumber))
-    } else if (STATE == "PAIR") {
-    	
-    } else {
-    	
-    }
-})
 input.onButtonPressed(Button.B, function () {
     if (STATE == "INIT") {
+        if (id == -1) {
+            id = 0
+        }
         Switch_State("PAIR")
+    }
+})
+function Pair_Radio (_name: string, _value: number, _serialnumber: number) {
+    if (_name == "HS" && _value == control.deviceSerialNumber()) {
+        basic.showString("AYYYYYY!!!")
+        if (Endpoint) {
+            Switch_State("ENDPOINT")
+        } else {
+            Switch_State("RELAY")
+        }
+    }
+}
+radio.onReceivedValue(function (name, value) {
+    if (STATE == "INIT") {
+        Init_Radio(name, value, radio.receivedPacket(RadioPacketProperty.SerialNumber))
+    } else if (STATE == "PAIR") {
+        Pair_Radio(name, value, radio.receivedPacket(RadioPacketProperty.SerialNumber))
+    } else {
+    	
     }
 })
 function Init () {
@@ -35,9 +57,10 @@ function Init () {
 }
 let Endpoint = false
 let STATE = ""
+let id = 0
 radio.setGroup(154)
 radio.setTransmitSerialNumber(true)
-let id = -1
+id = -1
 Switch_State("INIT")
 basic.forever(function () {
     if (STATE == "INIT") {
