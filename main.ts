1daea364d14166e5627f7c19abba3985beded11f
G433N <44841986+G433N@users.noreply.github.com>
2023-09-14 11:38:51 +0200

    fucking message shiot gjhiojfnd EOGIJNK

--- main.ts
+++ main.ts
@@ -16,8 +16,23 @@ function read_message (_message: string) {
 // "reciver_id",
 // "sender_id",
 // "message"
-function send_message (message_data: any[]) {
-    radio.sendString("")
+function send_message (message_data: number[]) {
+    if (!(message_data.length == 3)) {
+        return
+    }
+    message_str_string = []
+    for (let value of message_data) {
+        message_str_string.unshift(convertToText(value))
+    }
+    message_str_string.reverse()
+    message_str_string[0] = add_zeros_to_str(message_str_string[0], 1)
+    message_str_string[1] = add_zeros_to_str(message_str_string[1], 1)
+    message_str_string[2] = add_zeros_to_str(message_str_string[2], 2)
+    result_str = ""
+    for (let value2 of message_str_string) {
+        result_str = "" + result_str + value2
+    }
+    radio.sendString(result_str)
 }
 input.onButtonPressed(Button.A, function () {
     if (STATE == "INIT") {
@@ -47,12 +62,22 @@ function Switch_State (_state: string) {
     STATE = _state
     basic.showString(_state)
 }
+function repeting_zeros (num: number) {
+    zero_str = ""
+    for (let index = 0; index <= num; index++) {
+        zero_str = "0" + zero_str
+    }
+    return zero_str
+}
 radio.onReceivedString(function (receivedString) {
     if (STATE == "RELAY") {
-        let list: number[] = []
-        message_data = receivedString
-        basic.showString("Do shit here")
-        send_message(list)
+        message_data = read_message(receivedString)
+        dir = message_data[0] - message_data[1]
+        message_data[0] = message_data[0] + dir
+        message_data[1] = message_data[1] + dir
+        send_message(message_data)
+    } else if (STATE == "ENDPOINT") {
+    	
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -81,17 +106,103 @@ radio.onReceivedValue(function (name, value) {
         Pair_Radio(name, value, radio.receivedPacket(RadioPacketProperty.SerialNumber))
     }
 })
-let message_data = ""
-let id = 0
+function add_zeros_to_str (text: string, len: number) {
+    return "" + repeting_zeros(len - text.length) + text
+}
+let dir = 0
+let message_data: number[] = []
+let zero_str = ""
 let temp_id = 0
 let Endpoint = false
 let STATE = ""
+let result_str = ""
+let message_str_string: string[] = []
 let message_int = 0
 let sender_id = 0
 let reciver_id = 0
-for (let value of read_message("1122333")) {
-    basic.showString("" + (value))
-}
+let id = 0
+let character_list = [
+"A",
+"B",
+"B",
+"D",
+"E",
+"F",
+"G",
+"H",
+"I",
+"J",
+"K",
+"L",
+"M",
+"N",
+"O",
+"P",
+"Q",
+"R",
+"S",
+"T",
+"U",
+"V",
+"W",
+"X",
+"Y",
+"Z",
+"1",
+"2",
+"3",
+"4",
+"5",
+"6",
+"7",
+"8",
+"9",
+"0"
+]
+let base_ten_list = [
+5,
+67,
+70,
+22,
+1,
+43,
+25,
+40,
+4,
+53,
+23,
+49,
+8,
+7,
+26,
+52,
+77,
+16,
+13,
+2,
+14,
+41,
+17,
+68,
+71,
+76,
+161,
+134,
+125,
+122,
+121,
+202,
+229,
+238,
+241,
+242
+]
+radio.setGroup(169)
+radio.setTransmitSerialNumber(true)
+id = -1
+Switch_State("INIT")
 basic.forever(function () {
-	
+    if (STATE == "PAIR") {
+        Pair()
+    }
 })
